5d9ca69298e360480c59092ce4aca49a1a308497
Yusuke Tanaka <y.tanaka@aktsk.jp>
2014-10-02 11:13:47 +0900

    fix page1 duplicate bug

--- home/bin/gh-issues.rb
+++ home/bin/gh-issues.rb
@@ -1,21 +1,24 @@
 #!/usr/bin/env ruby
 require 'octokit'
+require 'active_support/core_ext'
 require 'csv'
 require 'pry'
 
 repo = ARGV[0]
 gc = Octokit::Client.new(access_token: ENV['GITHUB_ACCESS_TOKEN'])
 
-page_num = 0
-CSV.open(ARGV[1], "wb") do |csv|
-  csv << [:number, :title, :label, :state, :closed_at, :milestone, :assignee, :body, :link, :created_at]
+page_num = 1
+CSV.open(ARGV[1], "w") do |csv|
+  csv << [:number, :title, :label, :state, :milestone, :assignee, :body, :link, :created_at, :closed_at]
   while !(issues = gc.issues(repo, state: :all, direction: :asc, page: page_num)).empty? do
     issues.each do |i|
       next if i[:pull_request]
       milestone = i[:milestone] ? i[:milestone][:title] : ""
       assignee = i[:assignee] ? i[:assignee][:login] : ""
       label = i[:labels].empty? ? "" : i[:labels].first.name.encode("Shift_JIS")
-      csv << [i[:number], i[:title].to_s.encode("Shift_JIS"), label, i[:state], i[:closed_at], milestone, assignee, i[:body].to_s.encode("Shift_JIS"), i[:html_url], i[:created_at].getlocal]
+      body = i[:body].to_s.truncate(100).gsub(/(\r\n|\r|\n)/, ' ').encode("Shift_JIS")
+      closed_at = i[:closed_at] ? i[:closed_at].getlocal : nil
+      csv << [i[:number], i[:title].to_s.encode("Shift_JIS"), label, i[:state], milestone, assignee, body, i[:html_url], i[:created_at].getlocal, closed_at]
     end
     page_num = page_num + 1
   end
