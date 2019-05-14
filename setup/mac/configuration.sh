409fedc954c67c27155f94ecaa7ad737deea10bc
Yusuke Tanaka <y.tanaka@aktsk.jp>
2019-05-14 12:53:06 +0900

    人間の死ぬのはいつも早すぎるか遅すぎるかよ。 でも一生はちゃんとけりがついてそこにあるのよ。 一本、線が引かれたからには総決算しなけりゃ。 あんたは、あんたの一生以外の何ものでもないのよ。
    -- サルトル --

--- setup/mac/configuration.sh
+++
@@ -1,2 +0,0 @@
-cd `dirname $0`/config
-ln -sf Karabiner/private.xml
--- setup/mac/update-tmux-powerline.sh
+++ setup/mac/update-tmux-powerline.sh
@@ -8,5 +8,3 @@ git fetch origin
 git reset --hard origin/master
 
 ln -sf ${SCRIPT_DIR}/config/tmux-powerline-theme.sh ${SCRIPT_DIR}/../../homedot/tmux/tmux-powerline/themes/default.sh
-
-echo echo 'iTerm > Preferences > Profiles > Text Uncheck "Treat ambiguous-width characters as double width" at "Double-Width Characters".'
