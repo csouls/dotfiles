7ac6523ab48e27ffacd7f8e852d3c30bc149ddd8
Yusuke Tanaka <y.tanaka@aktsk.jp>
2021-08-05 18:31:36 +0900

    人間の運命は、 ルール通りに行われるチェスというよりむしろ宝くじを思い起こさせる。
    -- エレンブルグ --

--- home/bin/aws-get-caller.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-AWS_ACCESS_KEY_ID=$1 AWS_SECRET_ACCESS_KEY=$2 aws sts get-caller-identity
-if [ $? -eq 0 ]; then
-  AWS_ACCESS_KEY_ID=$1 AWS_SECRET_ACCESS_KEY=$2 aws iam list-access-keys
-  AWS_ACCESS_KEY_ID=$1 AWS_SECRET_ACCESS_KEY=$2 aws iam list-account-aliases
-fi
